d64d843b137d9864ccaa4b2a4c07fe4e612f7119
denefee <dsher2004@gmail.com>
2023-05-17 23:28:21 +0300

    hash

--- hash/LinkedList.h
+++ hash/LinkedList.h
@@ -1,47 +1,70 @@
+#include <iostream>
+
 struct LinkedList {
-	int data;
-	LinkedList* link;
+    int data;
+    LinkedList* next;
 };
 
-LinkedList* push(LinkedList* current, int data) {
-	if (nullptr == current) {
-		return new LinkedList{ data, nullptr };
-	}
-	LinkedList* temp = current;
-	while (temp->link != nullptr) {
-		temp = temp->link;
-	}
-	LinkedList* new_link = new LinkedList{ data, nullptr };
-	temp->link = new_link;
-	return new_link;
+LinkedList* insert_after(LinkedList*  const current, int data) {
+    LinkedList* new_node = new LinkedList{data, nullptr};
+    if (current != nullptr) {
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+    return new_node;
 }
 
-LinkedList* remove_from_head(LinkedList* const head, int x) {
-	auto cur = head;
-	if (head->data == x) {
-		delete head;
-		return nullptr;
-	}
-	LinkedList* prev = nullptr;
-	while (cur->data != x) {
-		if (cur->link == nullptr) {
-			return head;
-		}
-		prev = cur;
-		cur = cur->link;
-	}
-	prev->link = cur->link;
-	delete cur;
-	return head;
+LinkedList* remove_after(LinkedList* const current) {
+    if (current == nullptr || current->next == nullptr) {
+        return nullptr;
+    }
+    LinkedList* removed_node = current->next;
+    current->next = removed_node->next;
+    delete removed_node;
+    return current->next;
+}
+
+LinkedList* remove_value (LinkedList *head, int key) {
+    if (head == nullptr) return nullptr;
+    if (head != nullptr and head->data == key) {
+        auto tmp = head->next;
+        delete head;
+        return tmp;
+    }
+    LinkedList* current = head;
+    while (current->next != nullptr) {
+        if (current->next->data == key) remove_after(current);
+    }
+    return head;
 }
 
 void erase(LinkedList* const head) {
-	LinkedList* current = head;
-	while (current->link != nullptr) {
-		LinkedList* temp_link = current->link;
-		current->link = nullptr;
-		delete current;
-		current = temp_link;
-	}
-	delete current;
+    LinkedList* current = head;
+    while (current != nullptr) {
+        LinkedList* to_remove = current;
+        current = current->next;
+        delete to_remove;
+    }
+}
+
+LinkedList* reverse_rec(LinkedList* const head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+    LinkedList* new_head = reverse_rec(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return new_head;
+}
+
+LinkedList* reverse_itr(LinkedList* const head) {
+    LinkedList* prev = nullptr;
+    LinkedList* current = head;
+    while (current != nullptr) {
+        LinkedList* next_node = current->next;
+        current->next = prev;
+        prev = current;
+        current = next_node;
+    }
+    return prev;
 }
